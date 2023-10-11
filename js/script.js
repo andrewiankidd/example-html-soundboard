24e58104dda197726d41646d118b5469248440f1
andrewiankidd <contact@andrewiankidd.co.uk>
2023-10-11 18:10:01 +0000

    Deploying to gh-pages from @ andrewiankidd/example-html-soundboard@75e0b6d13694e7d479d82ec574986afd9f338a41 🚀

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 // wait for page to load
 document.addEventListener("DOMContentLoaded", function(_event) {
 
+    // add view source button
+    document.body.innerHTML += `<div id="viewSource"><a id="viewSourceHyperlink">View Source On GitHub</a></div>`
+    const viewSourceButton = document.getElementById('viewSourceHyperlink');
+    const ghUser = window.location.host.split('.')[0];
+    const ghProject = window.location.pathname.split('/').filter(e => e)[0]
+    viewSourceButton.href = `https://github.com/${ghUser}/${ghProject}`
+
     // get all buttons inside the soundboard element
     const buttons = document.querySelectorAll('#soundboard button');
 
@@ -11,18 +18,13 @@ document.addEventListener("DOMContentLoaded", function(_event) {
         const audio = button.querySelector('audio');
 
         // when the user clicks the button
+        console.log('Adding click handler to button', button)
         button.addEventListener("click", function() {
 
             // restart and play the audio
+            console.log('Button clicked', button)
             audio.currentTime = 0
             audio.play();
         });
     })
-
-    // view source button
-    document.body.innerHTML += `<div id="viewSource"><a id="viewSourceHyperlink">View Source On GitHub</a></div>`
-    const viewSourceButton = document.getElementById('viewSourceHyperlink');
-    const ghUser = window.location.host.split('.')[0];
-    const ghProject = window.location.pathname.split('/').filter(e => e)[0]
-    viewSourceButton.href = `https://github.com/${ghUser}/${ghProject}`
 });
